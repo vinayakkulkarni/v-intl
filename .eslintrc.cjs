8d01f5c6334545e62042c6a1c36a97d1a19fdd0e
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-11-08 02:31:43 +0530

    feat: use `cjs`
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2021,
+    sourceType: 'module',
+    lib: ['es2021'],
+    ecmaFeatures: {
+      jsx: true,
+      tsx: true,
+    },
+    extraFileExtensions: ['.vue'],
+  },
+  plugins: ['vue', 'jsdoc', 'security', '@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:jsdoc/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:vue/vue3-recommended',
+    'plugin:security/recommended',
+    'plugin:prettier/recommended',
+    'plugin:storybook/recommended',
+    'prettier',
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+  },
+};
