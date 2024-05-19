1f67ea749c355af1968a2655c5c616ca99a1ea7b
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2024-05-19 12:08:57 +0530

    feat(duration-format): add Duration Format component 🎉
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

---
+++ src/components/VIntlDurationFormat.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+  import { computed, defineModel } from 'vue';
+  import { DurationFormat } from '@formatjs/intl-durationformat';
+  import type {
+    DurationFormatOptions,
+    DurationInput,
+  } from '@formatjs/intl-durationformat/src/types';
+
+  type FormatOptions = {
+    locales?: string | string[];
+    options?: DurationFormatOptions;
+  };
+
+  const payload = defineModel<DurationInput>('payload', {
+    required: true,
+  });
+
+  const format = defineModel<FormatOptions>('format', {
+    required: false,
+    default: () => ({
+      locales: 'en-US',
+      options: {},
+    }),
+  });
+  const wrapper = defineModel<string>('wrapper', {
+    required: false,
+    default: '',
+  });
+  const toParts = defineModel<boolean>('toParts', {
+    required: false,
+    default: false,
+  });
+
+  const formattedDuration = computed(() => {
+    const duration = new DurationFormat(
+      format.value.locales,
+      format.value.options,
+    );
+    return toParts.value
+      ? duration.formatToParts(payload.value)
+      : duration.format(payload.value);
+  });
+</script>
+
+<template>
+  <div :class="wrapper">
+    {{ formattedDuration }}
+  </div>
+</template>
