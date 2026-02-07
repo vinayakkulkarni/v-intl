2eda72a7c7b2d9dc9842b0d588aafa685cce5938
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2026-02-08 04:26:58 +0530

    refactor: update vite.config.ts for ESM-only output
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,21 @@
 import vue from '@vitejs/plugin-vue';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
-import pkg from './package.json';
-
-const banner = `/*!
-* ${pkg.name} v${pkg.version}
-* (c) ${new Date().getFullYear()} ${pkg.author.name}
-* @license ${pkg.license}
-*/`;
 
 export default defineConfig({
   plugins: [vue()],
   build: {
     target: 'esnext',
-    minify: 'esbuild',
     sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
-      name: 'VIntl',
-      formats: ['es', 'cjs', 'umd'],
+      formats: ['es'],
       fileName: 'v-intl',
     },
-    commonjsOptions: {
-      extensions: ['.js', '.ts', '.vue'],
-      exclude: 'src/**',
-      include: 'node_modules/**',
-    },
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
       external: ['vue'],
       output: {
         exports: 'named',
-        banner,
-        strict: true,
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          vue: 'vue',
-        },
       },
     },
   },
